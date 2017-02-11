8002e8022dbdef34d70cc3c3b1922d7cc8a0eac2
jbengtsson <jbngtssn@gmail.com>
2017-02-11 14:22:31 +0100

    Code clean-up.

--- thor/wrk/max_iv_uc.cc
+++ thor/wrk/max_iv_uc.cc
@@ -1,4 +1,4 @@
-#define NO 4
+#define NO 3
 
 #include "thor_lib.h"
 
@@ -22,10 +22,14 @@ public:
   void add_prm(const std::string Fname, const int n,
 	       const double bn_max, const double bn_scl);
   void ini_prm(double *bn, double *bn_lim);
-  void set_prm(const int k) const;
-  void clr_prm(const int k) const;
-  void set_bn(double *db2, double *b2, double &db2_max) const;
+  void set_prm_dep(const int k) const;
+  void clr_prm_dep(const int k) const;
+  void set_dprm(double *dbn, double *bn, double &dbn_max) const;
+  void set_prm(double *bn) const;
 };
+
+
+param_type b2_prms;
 
 
 void param_type::add_prm(const std::string Fname, const int n,
@@ -59,7 +63,7 @@ void param_type::ini_prm(double *bn, double *bn_lim)
 }
 
 
-void param_type::set_prm(const int k) const
+void param_type::set_prm_dep(const int k) const
 {
   int j;
 
@@ -73,7 +77,7 @@ void param_type::set_prm(const int k) const
 }
 
 
-void param_type::clr_prm(const int k) const
+void param_type::clr_prm_dep(const int k) const
 {
   int j;
 
@@ -87,28 +91,43 @@ void param_type::clr_prm(const int k) const
 }
 
 
-void param_type::set_bn(double *db2, double *b2, double &db2_max) const
+void param_type::set_dprm(double *dbn, double *bn, double &dbn_max) const
 {
   int i;
 
   printf("\n");
-  db2_max = 0e0;
+  dbn_max = 0e0;
   for (i = 1; i <= n_prm; i++) {
-    db2[i] *= step*bn_scl[i-1];
+    dbn[i] *= step*bn_scl[i-1];
     if (n[i-1] > 0) {
-      set_dbn(Fnum[i-1], n[i-1], db2[i]);
-      b2[i] = get_bn(Fnum[i-1], 1, n[i-1]);
+      set_dbn(Fnum[i-1], n[i-1], dbn[i]);
+      bn[i] = get_bn(Fnum[i-1], 1, n[i-1]);
     } else if (n[i-1] == -1) {
-      set_dL(Fnum[i-1], db2[i]);
-      b2[i] = get_L(Fnum[i-1], 1);
+      set_dL(Fnum[i-1], dbn[i]);
+      bn[i] = get_L(Fnum[i-1], 1);
     } else if (n[i-1] == -2) {
-      set_dbn_s(-Fnum[i-1], n[i-1], db2[i]);
-      b2[i] = get_bn_s(-Fnum[i-1], 1, n[i-1]);
+      set_dbn_s(-Fnum[i-1], n[i-1], dbn[i]);
+      bn[i] = get_bn_s(-Fnum[i-1], 1, n[i-1]);
     }
-    db2_max = max(fabs(db2[i]), db2_max);
-    printf("%10.5f", b2[i]);
+    dbn_max = max(fabs(dbn[i]), dbn_max);
+    printf("%10.5f", bn[i]);
   }
   printf("\n");
+}
+
+
+void param_type::set_prm(double *bn) const
+{
+  int i;
+
+  for (i = 1; i <= n_prm; i++) {
+    if (n[i-1] > 0)
+      set_bn(Fnum[i-1], n[i-1], bn[i]);
+    else if (n[i-1] == -1)
+      set_L(Fnum[i-1], bn[i]);
+    else if (n[i-1] == -2)
+      set_bn_s(-Fnum[i-1], n[i-1], bn[i]);
+  }
 }
 
 
@@ -121,25 +140,37 @@ void no_mpoles(void)
     if (elem[j].kind == Mpole)
       for (k = Sext; k < mpole_max; k++) {
 	//	cout << "zeroing " << elem[j].Name << endl;
-	set_bn(elem[j].Fnum, elem[j].Knum, k, 0.0);
+	set_bn(elem[j].Fnum, elem[j].Knum, k, 0e0);
       }
+}
+
+
+void get_twiss(const ss_vect<tps> &A)
+{
+  int          j;
+  double       dnu[2];
+  ss_vect<tps> A1;
+
+  // Include parameter dependence.
+  danot_(2);
+
+  A1 = A;
+  for (j = 1; j <= n_elem; j++) {
+    A1.propagate(j, j);
+    elem_tps[j-1].A1 = get_A_CS(2, A1, dnu);
+  }
 }
 
 
 void get_twiss(const double alpha[], const double beta[],
 	       const double eta[], const double etap[])
 {
-  int    j;
-  double dnu[2];
+  ss_vect<tps> A;
 
   // Include parameter dependence.
   danot_(2);
 
-  A1 = get_A(alpha, beta, eta, etap);
-  for (j = 1; j <= n_elem; j++) {
-    A1.propagate(j, j);
-    elem_tps[j-1].A1 = get_A_CS(2, A1, dnu);
-  }
+  A = get_A(alpha, beta, eta, etap); get_twiss(A);
 }
 
 
@@ -309,7 +340,7 @@ void fit_emit(param_type &b2_prms, const double eps_x,
   do {
     printf("\n");
     for (i = 1; i <= n_b2; i++) {
-      b2_prms.set_prm(i-1);
+      b2_prms.set_prm_dep(i-1);
 
       get_Map(); K = MapNorm(Map, g, A1, A0, Map_res, 1); nus = dHdJ(K);
 
@@ -325,7 +356,7 @@ void fit_emit(param_type &b2_prms, const double eps_x,
       for (j = 1; j <= m; j++)
 	A[j][i] *= b2_prms.bn_scl[i-1];
 
-      b2_prms.clr_prm(i-1);
+      b2_prms.clr_prm_dep(i-1);
     }
 
     m = 0;
@@ -339,7 +370,7 @@ void fit_emit(param_type &b2_prms, const double eps_x,
 
     SVD_lim(m, n_b2, A, b, b2_lim, b2_prms.svd_cut, b2, db2);
 
-    b2_prms.set_bn(db2, b2, db2_max);
+    b2_prms.set_dprm(db2, b2, db2_max);
   } while (db2_max > b2_prms.bn_tol);
 
   prt_emit(b2_prms, b2);
@@ -421,7 +452,7 @@ void fit_match(param_type &b2_prms,
 
   do {
     for (i = 1; i <= n_b2; i++) {
-      b2_prms.set_prm(i-1);
+      b2_prms.set_prm_dep(i-1);
 
       get_twiss(alpha0, beta0, eta0, etap0);
       AA_tp = elem_tps[n_elem-1].A1*tp_S(2, elem_tps[n_elem-1].A1);
@@ -438,7 +469,7 @@ void fit_match(param_type &b2_prms,
       for (j = 1; j <= m; j++)
 	A[j][i] *= b2_prms.bn_scl[i-1];
 
-      b2_prms.clr_prm(i-1);
+      b2_prms.clr_prm_dep(i-1);
     }
 
     m = 0;
@@ -453,7 +484,7 @@ void fit_match(param_type &b2_prms,
 
     SVD_lim(m, n_b2, A, b, b2_lim, b2_prms.svd_cut, b2, db2);
 
-    b2_prms.set_bn(db2, b2, db2_max);
+    b2_prms.set_dprm(db2, b2, db2_max);
   } while (db2_max > b2_prms.bn_tol);
 
   prt_match(b2_prms, b2);
@@ -464,25 +495,23 @@ void fit_match(param_type &b2_prms,
 }
 
 
-void opt_match(param_type &b2_prms, const double beta0_x, const double beta0_y)
+void opt_match(param_type &b2_prms,
+	       const double beta0_x, const double beta0_y,
+	       const double beta1_x, const double beta1_y)
 {
   // Minimize linear linear chromaticity for super period.
   // Lattice: super period.
 
   const int m_max = 10;
 
-  long int     loc;
+  long int     loc1, loc2, loc3;
   int          n_b2, i, j, m;
   double       **A, *b, *b2_lim, *b2, *db2;
   double       db2_max;
   tps          K_re, K_im;
-  ss_vect<tps> AA_tp, A_disp;
+  ss_vect<tps> AA_tp1, AA_tp2, A_disp;
 
-  const double scl_alpha = 1e1, scl_eta = 1e1, scl_ksi = 1e-4,
-               alpha0[] = {0e0,     0e0},
-               beta0[]  = {beta0_x, beta0_y},
-               eta0[]   = {0e0,     0e0},
-	       etap0[]  = {0e0,     0e0};
+  const double scl_eta = 1e0, scl_ksi = 1e-5;
 
   n_b2 = b2_prms.n_prm;
 
@@ -492,61 +521,67 @@ void opt_match(param_type &b2_prms, const double beta0_x, const double beta0_y)
   b2_prms.ini_prm(b2, b2_lim);
 
   // Exit of 2nd BM.
-  loc = get_loc(get_Fnum("bm"), 2);
+  loc1 = get_loc(get_Fnum("bm"), 2);
+  loc2 = get_loc(get_Fnum("sfh"), 1);
+  loc3 = n_elem;
 
   do {
     for (i = 1; i <= n_b2; i++) {
-      b2_prms.set_prm(i-1);
+      b2_prms.set_prm_dep(i-1);
 
       danot_(3);
       get_Map();
       danot_(4);
       K = MapNorm(Map, g, A1, A0, Map_res, 1);
       CtoR(K, K_re, K_im);
-      // Sets no to 2.
-      get_twiss(alpha0, beta0, eta0, etap0);
-      AA_tp = elem_tps[n_elem-1].A1*tp_S(2, elem_tps[n_elem-1].A1);
-      A_disp = elem_tps[loc-1].A1;
+      // Call to get_twiss sets no to 2.
+      get_twiss(A1);
+      A_disp = elem_tps[loc1-1].A1;
+      AA_tp1 = elem_tps[loc2-1].A1*tp_S(2, elem_tps[loc2-1].A1);
+      AA_tp2 = elem_tps[loc3-1].A1*tp_S(2, elem_tps[loc3-1].A1);
+      danot_(4);
 
       m = 0;
-      A[++m][i] = scl_alpha*h_ijklm_p(-AA_tp[x_], 0, 1, 0, 0, 0, 7);
-      A[++m][i] = scl_alpha*h_ijklm_p(-AA_tp[y_], 0, 0, 0, 1, 0, 7);
       A[++m][i] = scl_eta*h_ijklm_p(A_disp[x_],  0, 0, 0, 0, 1, 7);
       A[++m][i] = scl_eta*h_ijklm_p(A_disp[px_], 0, 0, 0, 0, 1, 7);
-      A[++m][i] = h_ijklm_p(AA_tp[x_], 1, 0, 0, 0, 0, 7);
-      A[++m][i] = h_ijklm_p(AA_tp[y_], 0, 0, 1, 0, 0, 7);
-      A[++m][i] = scl_ksi*h_ijklm_p(K_re, 1, 1, 0, 0, 1, 7);
-      A[++m][i] = scl_ksi*h_ijklm_p(K_re, 0, 0, 1, 1, 1, 7);
+      A[++m][i] = h_ijklm_p(AA_tp1[x_], 1, 0, 0, 0, 0, 7);
+      A[++m][i] = h_ijklm_p(AA_tp1[y_], 0, 0, 1, 0, 0, 7);
+      A[++m][i] = h_ijklm_p(AA_tp2[x_], 1, 0, 0, 0, 0, 7);
+      A[++m][i] = h_ijklm_p(AA_tp2[y_], 0, 0, 1, 0, 0, 7);
+      // A[++m][i] = scl_ksi*h_ijklm_p(K_re, 1, 1, 0, 0, 1, 7);
+      // A[++m][i] = scl_ksi*h_ijklm_p(K_re, 0, 0, 1, 1, 1, 7);
 
       for (j = 1; j <= m; j++)
 	A[j][i] *= b2_prms.bn_scl[i-1];
 
-      b2_prms.clr_prm(i-1);
+      b2_prms.clr_prm_dep(i-1);
     }
 
-    printf("\n%8.3f %8.3f %8.3f %8.3f %8.3f %8.3f\n",
+    printf("\n%10.3e %10.3e %8.3f %8.3f %8.3f %8.3f %10.3e %10.3e\n",
 	   h_ijklm(A_disp[x_],  0, 0, 0, 0, 1),
 	   h_ijklm(A_disp[px_], 0, 0, 0, 0, 1),
-	   h_ijklm(-AA_tp[x_], 0, 1, 0, 0, 0),
-	   h_ijklm(-AA_tp[y_], 0, 0, 0, 1, 0),
-	   h_ijklm(AA_tp[x_], 1, 0, 0, 0, 0),
-	   h_ijklm(AA_tp[y_], 0, 0, 1, 0, 0));
+	   h_ijklm(AA_tp1[x_], 1, 0, 0, 0, 0),
+	   h_ijklm(AA_tp1[y_], 0, 0, 1, 0, 0),
+	   h_ijklm(AA_tp2[x_], 1, 0, 0, 0, 0),
+	   h_ijklm(AA_tp2[y_], 0, 0, 1, 0, 0),
+	   h_ijklm(K_re, 1, 1, 0, 0, 1),
+	   h_ijklm(K_re, 0, 0, 1, 1, 1));
 
     m = 0;
-    b[++m] = -scl_alpha*h_ijklm(-AA_tp[x_], 0, 1, 0, 0, 0);
-    b[++m] = -scl_alpha*h_ijklm(-AA_tp[y_], 0, 0, 0, 1, 0);
     b[++m] = -scl_eta*h_ijklm(A_disp[x_],  0, 0, 0, 0, 1);
     b[++m] = -scl_eta*h_ijklm(A_disp[px_], 0, 0, 0, 0, 1);
-    b[++m] = -(h_ijklm(AA_tp[x_], 1, 0, 0, 0, 0)-beta0_x);
-    b[++m] = -(h_ijklm(AA_tp[y_], 0, 0, 1, 0, 0)-beta0_y);
-    b[++m] = -scl_ksi*(h_ijklm(K_re, 1, 1, 0, 0, 1));
-    b[++m] = -scl_ksi*(h_ijklm(K_re, 0, 0, 1, 1, 1));
+    b[++m] = -(h_ijklm(AA_tp1[x_], 1, 0, 0, 0, 0)-beta1_x);
+    b[++m] = -(h_ijklm(AA_tp1[y_], 0, 0, 1, 0, 0)-beta1_y);
+    b[++m] = -(h_ijklm(AA_tp2[x_], 1, 0, 0, 0, 0)-beta0_x);
+    b[++m] = -(h_ijklm(AA_tp2[y_], 0, 0, 1, 0, 0)-beta0_y);
+    // b[++m] = -scl_ksi*(h_ijklm(K_re, 1, 1, 0, 0, 1));
+    // b[++m] = -scl_ksi*(h_ijklm(K_re, 0, 0, 1, 1, 1));
 
     prt_system(m, n_b2, A, b);
 
     SVD_lim(m, n_b2, A, b, b2_lim, b2_prms.svd_cut, b2, db2);
 
-    b2_prms.set_bn(db2, b2, db2_max);
+    b2_prms.set_dprm(db2, b2, db2_max);
   } while (db2_max > b2_prms.bn_tol);
 
   prt_match(b2_prms, b2);
@@ -554,6 +589,109 @@ void opt_match(param_type &b2_prms, const double beta0_x, const double beta0_y)
   free_dvector(b, 1, m_max); free_dvector(b2_lim, 1, n_b2);
   free_dvector(b2, 1, n_b2); free_dvector(db2, 1, n_b2);
   free_dmatrix(A, 1, m_max, 1, n_b2);
+}
+
+
+double f_match(double *b2)
+{
+  // Minimize linear linear chromaticity for super period.
+  // Lattice: super period.
+
+  static double chi2_ref = 1e30;
+
+  long int     i, loc1, loc2, loc3;
+  double       tr[2], chi2;
+  tps          K_re, K_im;
+  ss_vect<tps> AA_tp1, AA_tp2, A_disp;
+
+  const double scl_eta = 1e0, scl_ksi = 1e-1,
+	       beta0[] = {3.0,     3.0},
+               beta1[] = {1.35632, 1.91473};
+
+  b2_prms.set_prm(b2);
+
+  // Exit of 2nd BM.
+  loc1 = get_loc(get_Fnum("bm"), 2);
+  loc2 = get_loc(get_Fnum("sfh"), 1);
+  loc3 = n_elem;
+
+  danot_(2);
+  get_Map();
+  danot_(3);
+  K = MapNorm(Map, g, A1, A0, Map_res, 1);
+  CtoR(K, K_re, K_im);
+  // Call to get_twiss sets no to 2.
+  get_twiss(A1);
+  A_disp = elem_tps[loc1-1].A1;
+  AA_tp1 = elem_tps[loc2-1].A1*tp_S(2, elem_tps[loc2-1].A1);
+  AA_tp2 = elem_tps[loc3-1].A1*tp_S(2, elem_tps[loc3-1].A1);
+
+  tr[X_] = h_ijklm(Map[x_], 1, 0, 0, 0, 0)+h_ijklm(Map[px_], 0, 1, 0, 0, 0);
+  tr[Y_] = h_ijklm(Map[y_], 0, 0, 1, 0, 0)+h_ijklm(Map[py_], 0, 0, 0, 1, 0);
+  // printf("trace: %6.3f %6.3f\n", tr[X_], tr[Y_]);
+
+  if ((fabs(tr[X_]) < 2e0) && (fabs(tr[Y_]) < 2e0)) {
+    chi2 = 0e0;
+    chi2 += sqr(scl_eta*h_ijklm(A_disp[x_],  0, 0, 0, 0, 1));
+    chi2 += sqr(scl_eta*h_ijklm(A_disp[px_], 0, 0, 0, 0, 1));
+    chi2 += sqr((h_ijklm(AA_tp1[x_], 1, 0, 0, 0, 0)-beta1[X_]));
+    chi2 += sqr((h_ijklm(AA_tp1[y_], 0, 0, 1, 0, 0)-beta1[Y_]));
+    chi2 += sqr((h_ijklm(AA_tp2[x_], 1, 0, 0, 0, 0)-beta0[X_]));
+    chi2 += sqr((h_ijklm(AA_tp2[y_], 0, 0, 1, 0, 0)-beta0[Y_]));
+    chi2 += sqr(scl_ksi*(h_ijklm(K_re, 1, 1, 0, 0, 1)));
+    chi2 += sqr(scl_ksi*(h_ijklm(K_re, 0, 0, 1, 1, 1)));
+  } else
+    chi2 = 1e10;
+
+  if (chi2 < chi2_ref) {
+    printf("\nchi2: %12.5e, %12.5e\n", chi2, chi2_ref);
+    printf("b:    %10.3e %10.3e %8.3f %8.3f %8.3f %8.3f %10.3e %10.3e\n",
+	   h_ijklm(A_disp[x_],  0, 0, 0, 0, 1),
+	   h_ijklm(A_disp[px_], 0, 0, 0, 0, 1),
+	   h_ijklm(AA_tp1[x_], 1, 0, 0, 0, 0),
+	   h_ijklm(AA_tp1[y_], 0, 0, 1, 0, 0),
+	   h_ijklm(AA_tp2[x_], 1, 0, 0, 0, 0),
+	   h_ijklm(AA_tp2[y_], 0, 0, 1, 0, 0),
+	   h_ijklm(K_re, 1, 1, 0, 0, 1),
+	   h_ijklm(K_re, 0, 0, 1, 1, 1));
+    printf("b2s: ");
+    for (i = 1; i <= b2_prms.n_prm; i++)
+      printf("%9.5f", b2[i]);
+    printf("\n");
+  }
+
+  chi2_ref = min(chi2, chi2_ref);
+
+  return chi2;
+}
+
+
+void opt_match1(param_type &b2_prms)
+{
+  // Minimize linear linear chromaticity for super period.
+  // Lattice: super period.
+
+  int    n_b2, i, j, iter;
+  double *b2, *b2_lim, **xi, fret;
+
+  n_b2 = b2_prms.n_prm;
+
+  b2 = dvector(1, n_b2); b2_lim = dvector(1, n_b2);
+  xi = dmatrix(1, n_b2, 1, n_b2);
+
+  b2_prms.ini_prm(b2, b2_lim);
+
+  // Set initial directions (unit vectors).
+  for (i = 1; i <= n_b2; i++)
+    for (j = 1; j <= n_b2; j++)
+      xi[i][j] = (i == j)? 1e0 : 0e0;
+
+  dpowell(b2, xi, n_b2, 1e-15, &iter, &fret, f_match);
+
+  prt_match(b2_prms, b2);
+
+  free_dvector(b2, 1, n_b2);  free_dvector(b2_lim, 1, n_b2);
+  free_dmatrix(xi, 1, n_b2, 1, n_b2);
 }
 
 
@@ -668,8 +806,8 @@ int main(int argc, char *argv[])
   double           Jx, Jy;
   tps              eps1_x, K_re, K_im, g_re, g_im, h_re, h_im, H_re, H_im;
   ss_vect<tps>     Id_scl;
-  param_type       b2_prms, b3_prms;
-  std::vector<int> b2_Fams, bn_Fams;
+  param_type       b3_prms;
+  std::vector<int> bn_Fams;
   std::ofstream    outf;
   
   const double max_Ax = 5e-3, max_Ay = 5e-3, max_delta = 3e-2;
@@ -678,7 +816,8 @@ int main(int argc, char *argv[])
                nu[]    = {4.0/15.0, 1.0/15.0},
                beta0[] = {0.38133, 5.00190},
 	       eta0_x  = 0.00382435,
-               beta1[] = {3.0, 3.0};
+	       beta1[] = {3.0,     3.0},
+               beta2[] = {1.35632, 1.91473};
 	    
 
   rad_on    = false; H_exact        = false; totpath_on   = false;
@@ -719,8 +858,6 @@ int main(int argc, char *argv[])
     b2_prms.bn_tol = 1e-4; b2_prms.svd_cut = 1e-6;  b2_prms.step = 0.8;
 
     fit_emit(b2_prms, eps_x, nu[X_], nu[Y_]);
-
-    prt_mfile("flat_file.dat");
   }
 
   if (false) {
@@ -742,23 +879,24 @@ int main(int argc, char *argv[])
   if (true) {
     b2_prms.add_prm("bm",   2, 25.0, 1.0);
     b2_prms.add_prm("qfe",  2, 25.0, 1.0);
-    b2_prms.add_prm("qde",  2, 50.0, 1.0);
+    b2_prms.add_prm("qde",  2, 30.0, 1.0);
     b2_prms.add_prm("qm",   2, 25.0, 1.0);
 
-    b2_prms.add_prm("l5h", -1, 0.2,  0.1);
-    b2_prms.add_prm("l6",  -1, 0.2,  0.1);
-    b2_prms.add_prm("l7h", -1, 0.2,  0.1);
-    b2_prms.add_prm("l8",  -1, 0.2,  0.1);
+    b2_prms.add_prm("l5h", -1, 0.2,  0.01);
+    b2_prms.add_prm("l6",  -1, 0.2,  0.01);
+    b2_prms.add_prm("l7h", -1, 0.3,  0.01);
+    b2_prms.add_prm("l8",  -1, 0.2,  0.01);
 
-    // b2_prms.add_prm("bm",  -2,  0.2, 0.1);
-    // b2_prms.add_prm("qfe", -2,  0.2, 0.1);
-    // b2_prms.add_prm("qde", -2,  0.2, 0.1);
-    // b2_prms.add_prm("qm",  -2,  0.2, 0.1);
+    b2_prms.add_prm("bm",  -2,  0.2, 1.0);
+    b2_prms.add_prm("qfe", -2,  0.2, 1.0);
+    b2_prms.add_prm("qde", -2,  0.2, 1.0);
+    b2_prms.add_prm("qm",  -2,  0.2, 1.0);
 
-    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-6;  b2_prms.step = 0.4;
+    b2_prms.bn_tol = 1e-6; b2_prms.svd_cut = 1e-4; b2_prms.step = 0.001;
 
     no_mpoles();
-    opt_match(b2_prms, beta1[X_], beta1[Y_]);
+    // opt_match(b2_prms, beta1[X_], beta1[Y_], beta2[X_], beta2[Y_]);
+    opt_match1(b2_prms);
   }
 
   if (false) {
@@ -766,7 +904,7 @@ int main(int argc, char *argv[])
     // b3_prms.add_prm("o2", 3);
     // b3_prms.add_prm("o3", 3);
 
-    fit_3rd_achrom(bn_Fams, 1e5, 1e-5, 1e-4, 1.0);
+    fit_3rd_achrom(bn_Fams, 1e5, 1e-5, 1e-3, 1.0);
   }
 
   if (false) {
@@ -783,4 +921,6 @@ int main(int argc, char *argv[])
     file_wr(outf, "h.dat"); outf << h_re*Id_scl; outf.close();
     file_wr(outf, "H.dat"); outf << H_re*Id_scl; outf.close();
   }
+
+  prt_mfile("flat_file.dat");
 }
