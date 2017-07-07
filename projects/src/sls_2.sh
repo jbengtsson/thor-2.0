c640d17aad1806787f85dfe3592ba368fc4cc18d
Bengtsson <ria34843@cs04r-sc-serv-71.diamond.ac.uk>
2017-07-07 17:02:18 +0100

    Code tweaks.

--- projects/src/sls_2.sh
+++ projects/src/sls_2.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-queue="ap-medium.q"
+queue="ap-high.q"
+#queue="ap-medium.q"
 
-t1="00:10:00"
-t2="12:00:00"
+#t1="00:10:00"
+#t2="12:00:00"
 
 dir=$THOR_LIB/projects/src
 
 \rm sls_2.cmd.o*
 
-qsub -l h_rt=$t2 -q $queue $dir/sls_2.cmd
+#qsub -l h_rt=$t2 -q $queue $dir/sls_2.cmd
+qsub -q $queue $dir/sls_2.cmd
