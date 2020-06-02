6b5e2a2c44c186536640a99b4f820038ef1660dd
J.B <johan@JBs-MacBook-Pro.local>
2020-06-02 11:55:15 +0100

    Code tweaks.

--- projects/src/get_dnu.cc
+++ projects/src/get_dnu.cc
@@ -1,4 +1,4 @@
-#define NO 6
+#define NO 8
 
 #include "thor_lib.h"
 
@@ -421,72 +421,81 @@ void prt_drv_terms(const tps &h_re, const tps &h_im, const ss_vect<tps> &nus,
 		   const tps &g_re, const tps &g_im)
 {
   printf("\nLinear chromaticity:\n");
-  printf("  ksi1            = [%10.3e, %10.3e]\n",
+  printf("  ksi1             = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 0, 0, 1), h_ijklm(nus[4], 0, 0, 0, 0, 1));
 
   printf("\n3rd order chromatic terms:\n");
-  printf("  h_20001         = %10.3e\n",
+  printf("  h_20001          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 0, 0, 0, 1));
-  printf("  h_00201         = %10.3e\n",
+  printf("  h_00201          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 0, 0, 2, 0, 1));
-  printf("  h_10002         = %10.3e\n",
+  printf("  h_10002          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 0, 0, 0, 2));
 
   printf("\n3rd order geometric terms:\n");
-  printf("  h_30000         = %10.3e\n",
+  printf("  h_30000          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 3, 0, 0, 0, 0));
-  printf("  h_21000         = %10.3e\n",
+  printf("  h_21000          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 1, 0, 0, 0));
-  printf("  h_10200         = %10.3e\n",
+  printf("  h_10200          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 0, 2, 0, 0));
-  printf("  h_10110         = %10.3e\n",
+  printf("  h_10110          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 0, 1, 1, 0));
-  printf("  h_10020         = %10.3e\n",
+  printf("  h_10020          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 0, 0, 2, 0));
  
   printf("\n4th order geometric terms:\n");
-  printf("  h_40000         = %10.3e\n",
+  printf("  h_40000          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 4, 0, 0, 0, 0));
-  printf("  h_31000         = %10.3e\n",
+  printf("  h_31000          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 3, 1, 0, 0, 0));
-  printf("  h_22000         = %10.3e\n",
+  printf("  h_22000          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 2, 0, 0, 0));
-  printf("  h_20200         = %10.3e\n",
+  printf("  h_20200          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 0, 2, 0, 0));
-  printf("  h_11200         = %10.3e\n",
+  printf("  h_11200          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 1, 2, 0, 0));
-  printf("  h_20110         = %10.3e\n",
+  printf("  h_20110          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 0, 1, 1, 0));
-  printf("  h_11110         = %10.3e\n",
+  printf("  h_11110          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 1, 1, 1, 1, 0));
-  printf("  h_00310         = %10.3e\n",
+  printf("  h_00310          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 0, 0, 3, 1, 0));
-  printf("  h_20020         = %10.3e\n",
+  printf("  h_20020          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 2, 0, 0, 2, 0));
-  printf("  h_00400         = %10.3e\n",
+  printf("  h_00400          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 0, 0, 4, 0, 0));
-  printf("  h_00220         = %10.3e\n",
+  printf("  h_00220          = %10.3e\n",
 	 h_abs_ijklm(h_re, h_im, 0, 0, 2, 2, 0));
 
   printf("\nTune footprint:\n");
-  printf("  nu(deltaˆ2)     = [%10.3e, %10.3e]\n",
+  printf("  nu(deltaˆ2)      = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 0, 0, 2), h_ijklm(nus[4], 0, 0, 0, 0, 2));
-  printf("  nu(deltaˆ3)     = [%10.3e, %10.3e]\n",
+  printf("  nu(deltaˆ3)      = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 0, 0, 3), h_ijklm(nus[4], 0, 0, 0, 0, 3));
-  printf("  nu(deltaˆ4)     = [%10.3e, %10.3e]\n",
+  printf("  nu(deltaˆ4)      = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 0, 0, 4), h_ijklm(nus[4], 0, 0, 0, 0, 4));
 
-  printf("\n  nu(2J_x)        = [%10.3e, %10.3e]\n",
+  printf("\n  nu(2J_x)         = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 1, 1, 0, 0, 0), h_ijklm(nus[4], 1, 1, 0, 0, 0));
-  printf("  nu(2J_y)        = [%10.3e, %10.3e]\n",
+  printf("  nu(2J_y)         = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 1, 1, 0), h_ijklm(nus[4], 0, 0, 1, 1, 0));
 
-  printf("\n  nu(2J_xˆ2)      = [%10.3e, %10.3e]\n",
+  printf("\n  nu(2J_xˆ2)       = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 2, 2, 0, 0, 0), h_ijklm(nus[4], 2, 2, 0, 0, 0));
-  printf("  nu(2J_x, 2J_y)  = [%10.3e, %10.3e]\n",
+  printf("  nu(2J_x, 2J_y)   = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 1, 1, 1, 1, 0), h_ijklm(nus[4], 1, 1, 1, 1, 0));
-  printf("  nu(2J_yˆ2)      = [%10.3e, %10.3e]\n",
+  printf("  nu(2J_yˆ2)       = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus[3], 0, 0, 2, 2, 0), h_ijklm(nus[4], 0, 0, 2, 2, 0));
+
+  printf("\n  nu(2J_xˆ3)       = [%10.3e, %10.3e]\n",
+	 h_ijklm(nus[3], 3, 3, 0, 0, 0), h_ijklm(nus[4], 3, 3, 0, 0, 0));
+  printf("  nu(2J_xˆ2, 2J_y) = [%10.3e, %10.3e]\n",
+	 h_ijklm(nus[3], 2, 2, 1, 1, 0), h_ijklm(nus[4], 2, 2, 1, 1, 0));
+  printf("  nu(2J_x, 2J_yˆ2) = [%10.3e, %10.3e]\n",
+	 h_ijklm(nus[3], 1, 1, 2, 2, 0), h_ijklm(nus[4], 1, 1, 2, 2, 0));
+  printf("  nu(2J_yˆ3)       = [%10.3e, %10.3e]\n",
+	 h_ijklm(nus[3], 0, 0, 3, 3, 0), h_ijklm(nus[4], 0, 0, 3, 3, 0));
 
   printf("\n  nu(2J_x, delta) = [%10.3e, %10.3e]\n",
 	 h_ijklm(nus_delta[3], 1, 1, 0, 0, 1),
